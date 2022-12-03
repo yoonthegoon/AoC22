71202f86084dd4867316419e66cc35cfd1842d2d
yoonthegoon <71526721+yoonthegoon@users.noreply.github.com>
2022-12-03 00:17:30 -0500

    solve day 3

---
+++ puzzles/day_03/solution.py
@@ -0,0 +1,36 @@
+with open('input.txt', 'r') as f:
+    INPUT = f.read()
+
+
+RUCKSACKS = INPUT.split('\n')[:-1]
+PRIORITIES = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def p1():
+    priorities_sum = 0
+
+    for rucksack in RUCKSACKS:
+        half = len(rucksack) // 2
+        compartment1, compartment2 = rucksack[:half], rucksack[half:]
+        for item in compartment1:
+            if item in compartment2:
+                priorities_sum += PRIORITIES.index(item)
+                break
+
+    return priorities_sum
+
+
+def p2():
+    priorities_sum = 0
+
+    groups = [RUCKSACKS[i:i + 3] for i in range(0, len(RUCKSACKS), 3)]
+    for group in groups:
+        for item in group[0]:
+            if item in group[1] and item in group[2]:
+                priorities_sum += PRIORITIES.index(item)
+                break
+
+    return priorities_sum
+
+
+if __name__ == '__main__':
+    print(p1(), p2())
