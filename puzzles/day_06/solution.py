248ef8bd2056f19c6ebd36552c4d69c8c6c21b2b
yoon <71526721+yoonthegoon@users.noreply.github.com>
2022-12-06 09:06:59 -0500

    solve day 6

---
+++ puzzles/day_06/solution.py
@@ -0,0 +1,30 @@
+import re
+
+
+with open('input.txt', 'r') as f:
+    INPUT = f.read()
+
+
+def start_of_marker(characters: int) -> int:
+    marker = False
+    for i, _ in enumerate(INPUT):
+        signals = INPUT[i:i + characters]
+        for signal in signals:
+            if signals.count(signal) > 1:
+                marker = False
+                break
+            marker = True
+        if marker:
+            return i + characters
+
+
+def p1():
+    return start_of_marker(4)
+
+
+def p2():
+    return start_of_marker(14)
+
+
+if __name__ == '__main__':
+    print(p1(), p2())
