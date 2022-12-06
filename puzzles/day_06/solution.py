20bb8df763dfbd1e426fedda3a4d45e3cbfd9328
yoon <71526721+yoonthegoon@users.noreply.github.com>
2022-12-06 16:11:02 -0500

    add regex lmao

--- puzzles/day_06/solution.py
+++ puzzles/day_06/solution.py
@@ -1,26 +1,25 @@
+import re
+
+
 with open('input.txt', 'r') as f:
     INPUT = f.read()
 
 
-def start_of_marker(characters: int) -> int:
-    marker = False
-    for i, _ in enumerate(INPUT):
-        signals = INPUT[i:i + characters]
-        for signal in signals:
-            if signals.count(signal) > 1:
-                marker = False
-                break
-            marker = True
-        if marker:
-            return i + characters
+def re_factory(characters: int) -> str:
+    pattern = '(.)'
+    for i in range(1, characters):
+        group = "|".join([f'\\{i}' for i in range(1, i + 1)])
+        pattern += f'((?!{group})\\w)'
+
+    return pattern
 
 
 def p1():
-    return start_of_marker(4)
+    return re.search(re_factory(4), INPUT).span()[1]
 
 
 def p2():
-    return start_of_marker(14)
+    return re.search(re_factory(14), INPUT).span()[1]
 
 
 if __name__ == '__main__':
